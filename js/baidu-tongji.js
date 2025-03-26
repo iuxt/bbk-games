cded53cf99852916a8d2cf5814a6beb0f671c859
zhanglikun <x@zahui.fan>
2025-03-26 23:23:56 +0800

    删文件

--- js/baidu-tongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2b5ec15383f7d19996e3bd864580198d";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
